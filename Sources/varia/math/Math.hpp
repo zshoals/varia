c495b8cd800ff979ca5c1adbf0dc7bc6816e3f69
z <zshoals@gmail.com>
2022-12-11 16:00:14 -0600

    random math utilities

--- Sources/varia/math/Math.hpp
+++ Sources/varia/math/Math.hpp
@@ -5,6 +5,71 @@ namespace Varia
 namespace Math
 {
 
+constexpr double EPSILON = 0.00000001;
+constexpr double PI = 3.145926545;
+constexpr double TAU = PI * 2;
+
+constexpr inline float lerp(float value, float start, float end)
+{
+	return start * (1 - value) + (end * value);
+}
+
+constexpr inline float inverse_lerp(float value, float start, float end)
+{
+	return (value - start) / (end - start);
+}
+
+constexpr inline float remap(float value, float s1, float e1, float s2, float e2)
+{
+	return inverse_lerp(lerp(value, s1, e1), s2, e2);
+}
+
+constexpr inline float rad2deg(float radians)
+{
+	return radians * (180.0 / PI);
+}
+
+constexpr inline float deg2rad(float degrees)
+{
+	return degrees * (PI / 180.0);
+}
+
+constexpr inline float minF(float a, float b)
+{
+	return (a < b) ? a : b;
+}
+
+constexpr inline float maxF(float a, float b)
+{
+	return (a > b) ? a : b;
+}
+
+constexpr inline int min(int a, int b)
+{
+	return (a < b) ? a : b;
+}
+
+constexpr inline int max(int a, int b)
+{
+	return (a > b) ? a : b;
+}
+
+constexpr inline float clampF(float value, float lower, float upper)
+{
+	if (value < lower) value = lower;
+	if (value > upper) value = upper;
+
+	return value;
+}
+
+constexpr inline int clamp(int value, int lower, int upper)
+{
+	if (value < lower) value = lower;
+	if (value > upper) value = upper;
+
+	return value;
+}
+
 template <typename T>
 constexpr inline T mod(T a, T b)
 {
