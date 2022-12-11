37bff47cfe762c5e2b44e360319306578ce707a2
z <zshoals@gmail.com>
2022-12-11 12:58:04 -0600

    ease of use functions

--- Sources/varia/exd/Entity.hpp
+++ Sources/varia/exd/Entity.hpp
@@ -10,23 +10,34 @@ struct Entity
 {
 	u64 id;
 
-	inline void increment_generation(u8 generation_pivot)
+	inline void generation_increment(u8 generation_pivot)
 	{
-		vds::Bits64 id = {};
-		id.storage = this->id;
-		id.increment_upper(generation_pivot);
+		vds::Bits64 bitmode = {};
+		bitmode.storage = this->id;
+		bitmode.increment_upper(generation_pivot);
+		this->id = bitmode.storage;
 	}
 
-	inline u64 extract_id_exclusively(u8 generation_pivot)
+	inline u64 generation_extract(u8 generation_pivot)
 	{
-		vds::Bits64 id = {};
+		vds::Bits64 bitmode = {};
+		bitmode.storage = this->id;
+		bitmode.rotate_right(generation_pivot);
+
+		return bitmode.storage;
+	}
+
+	inline u64 id_extract(u8 generation_pivot)
+	{
+		vds::Bits64 bitmode = {};
+		bitmode.storage = this->id;
+
 		vds::Bits64 lo_mask = {};
-		lo_mask.not();
 		lo_mask.generate_bitmask_lo(generation_pivot);
 
-		id.mask_allow(lo_mask);
+		bitmode.mask_allow(lo_mask);
 
-		return id.storage;
+		return bitmode.storage;
 	}
 
 	inline bool matches(Entity other) { return this->id == other.id; }
