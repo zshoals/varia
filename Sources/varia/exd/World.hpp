9165131fe6be2d5e7b387865e7fec34854884854
z <zshoals@gmail.com>
2022-12-12 12:24:37 -0600

    finish autogeneration and location of bitsets

--- Sources/varia/exd/World.hpp
+++ Sources/varia/exd/World.hpp
@@ -3,6 +3,8 @@
 #include "Entity.hpp"
 #include "EntityManifest.hpp"
 #include "Component.hpp"
+#include "EXDUtil.hpp"
+#include "EXDConstants.hpp"
 #include "varia/ds/Bitset.hpp"
 #include "varia/math/Math.hpp"
 #include "varia/vcommon.hpp"
@@ -14,19 +16,6 @@
 namespace exd
 {
 
-namespace WorldUtil
-{
-	constexpr u8 id_shift(size_t entity_count)
-	{
-		size_t nearest = Varia::Math::pow2_next_nearest(entity_count);
-		u8 shift = Varia::Math::pow2_to_bitshift_value(nearest);
-
-		//Note(zshoals Dec-12-2022): The shift value is used for bitmasking
-		//This value should result in 
-		return shift - 1;
-	}
-}
-
 template<int Size>
 struct World
 {
@@ -34,27 +23,17 @@ struct World
 	//And also bitsets and tags
 
 	exd::EntityManifest<Size> entities;
-	constexpr static u8 id_shift = WorldUtil::id_shift(Size);
-
-	//====================================================
-	//           Import all component bitsets
-	//====================================================
-
-	#define internal_bitset_name VARIA_CONCAT(internal_ents_, FIELD_NAME)
-
-	//Note(zshoals Dec-11-2022): Disable warning 4005
-	//Macro redefinition warning, however we are intentionally overwriting the macro here
-	#pragma warning(push)
-	#pragma warning(disable: 4005)
-
-	#define COMPONENT_DATA(TYPE, FIELD_NAME) vds::Bitset32<Size> internal_bitset_name = {};
-	#include "ComponentData.def"
-
-	#pragma warning(pop)
+	//Note(zshoals Dec-12-2022): We just set an upper bound here...hacky but simple...
+	//500kb at 8192 entities at 512 bitsets
+	vds::Bitset32<Size> component_ents[exd::Constants::exd_max_components];
+	constexpr static u8 id_shift = EXDUtil::id_shift(Size);
 
 	//====================================================
 	//           Import all component data arrays
 	//====================================================
+
+	//Note(zshoals Dec-11-2022): Disable warning 4005
+	//Macro redefinition warning, however we are intentionally overwriting the macro here
 	#pragma warning(push)
 	#pragma warning(disable: 4005)
 
@@ -67,25 +46,50 @@ struct World
 	//          End bitsets and data arrays 
 	//====================================================
 
+	World(void)
+	{
+		//====================================================
+		//          Set all component bitset handles
+		//====================================================
+		size_t counter = 0;
+
+		#pragma warning(push)
+		#pragma warning(disable: 4005)
+
+		#define COMPONENT_DATA(TYPE, FIELD_NAME) FIELD_NAME.bitset_handle = counter; ++counter; 
+		#include "ComponentData.def"
+
+		#pragma warning(pop)
+	}
+
+
 	exd::Entity ent_create(void) { return entities.entity_get_free(); }
 	void ent_kill(exd::Entity ent) { entities.entity_release(ent, id_shift); internal_remove_from_components(ent); };
-	void ent_valid(exd::Entity ent) { entities.entity_valid(ent, id_shift); }
+	bool ent_valid(exd::Entity ent) { return entities.entity_valid(ent, id_shift); }
 	size_t ent_id(exd::Entity ent) { return ent.id_extract(id_shift); }
 	size_t ent_gen(exd::Entity ent) { return ent.generation_extract(id_shift); }
 
 	void internal_remove_from_components(exd::Entity ent)
 	{
 
-	//====================================================
-	//   Generate removal functions for all components
-	//====================================================
+		//====================================================
+		//   Generate removal functions for all components
+		//====================================================
 		#pragma warning(push)
 		#pragma warning(disable: 4005)
 
-		#define COMPONENT_DATA(TYPE, FIELD_NAME) internal_bitset_name.unset(ent.id_extract(id_shift));
+		size_t counter = 0;
+
+		#define COMPONENT_DATA(TYPE, FIELD_NAME) component_ents[counter].unset(ent.id_extract(id_shift)); ++counter;
 		#include "ComponentData.def"
 
 		#pragma warning(pop)
+	}
+
+	vds::Bitset32<Size> * internal_bitset_lookup(size_t idx)
+	{
+		DEBUG_ENSURE_UINT_LT(idx, exd::Constants::exd_max_components, "Overflow of range in component bitset selection.");
+		return &component_ents[idx];
 	}
 
 
