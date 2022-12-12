19b79b92122e3471b640833e617279c615da4e88
z <zshoals@gmail.com>
2022-12-12 12:23:11 -0600

    split out shared functionality

---
+++ Sources/varia/exd/EXDUtil.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "varia/vcommon.hpp"
+#include "varia/math/Math.hpp"
+
+namespace EXDUtil
+{
+	constexpr u8 id_shift(size_t entity_count)
+	{
+		size_t nearest = Varia::Math::pow2_next_nearest(entity_count);
+		u8 shift = Varia::Math::pow2_to_bitshift_value(nearest);
+
+		//Note(zshoals Dec-12-2022): The shift value is used for bitmasking
+		//This value should result in 
+		return shift - 1;
+	}
+}
