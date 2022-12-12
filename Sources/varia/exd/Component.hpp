1d6642f466c2292c460efe0a5c5fb79e5ed9464e
z <zshoals@gmail.com>
2022-12-12 12:22:08 -0600

    implement basic component accessors and global entity store safety checks

--- Sources/varia/exd/Component.hpp
+++ Sources/varia/exd/Component.hpp
@@ -1,9 +1,19 @@
 #pragma once
 
+#include "Entity.hpp"
+#include "World.hpp"
+#include "EXDUtil.hpp"
+#include "EXDConstants.hpp"
+
+#include "varia/ds/Bitset.hpp"
 #include "varia/ds/StaticArray.hpp"
 #include "varia/validation.hpp"
 
-#include "Entity.hpp"
+//Note(zshoals Dec-12-2022): Lame, but for convenient usage of components we have to 
+//break this circular dependency, otherwise life is going to get annoying real fast
+//Kind of bad architecture but we're interntionally doing weird things with the 
+//component bitsets that are stored in World
+template<int Size> struct World;
 
 namespace exd
 {
@@ -14,31 +24,74 @@ template<typename T, int Size>
 struct Component
 {
 	vds::StaticArray<T, Size> comps = {};
-	u8 id_bits = 0;
-	
-	Component(u8 id_bits) : id_bits{id_bits} {}
+	size_t bitset_handle = SIZE_MAX;
+	constexpr static u8 id_bits = EXDUtil::id_shift(Size);
 
-	T const * comp_get(Entity ent) 
+	T const * comp_get(World<Size> * w, Entity ent) 
 	{ 
-		//TODO(zshoals): This must perform a check against the global entity storage
-		//otherwise we can't confirm that the entities match in terms of generation
-		//for iterable entities, we can use an unchecked version instead
-		ENSURE_UNIMPLEMENTED();
-		return comps.get(ent.id_extract(id_bits)); 
+		vds::Bitset32<Size> * ent_states = w->internal_bitset_lookup(bitset_handle);
+		u64 id = ent.id_extract(id_bits);
+		bool alive = ent_states->is_set(id);
+		bool valid = w->ent_valid(ent);
+
+		if (alive && valid)
+		{
+			//Note(zshoals Dec-12-2022): We already know that the selected ID will be in bounds
+			//as everything in World is templated to a fixed size
+			return comps.get_unsafe(id);
+		}
+		else
+		{
+			return nullptr;
+		}
 	} 
 
-	T const * comp_get_unsafe(Entity ent) { ENSURE_UNIMPLEMENTED(); }
-
-	T * comp_get_mut(Entity ent) 
+	T * comp_get_mut(World<Size> * w, Entity ent) 
 	{ 
-		//TODO(zshoals): This must perform a check against the global entity storage
-		//otherwise we can't confirm that the entities match in terms of generation
-		//for iterable entities, we can use an unchecked version instead
-		ENSURE_UNIMPLEMENTED();
-		return comps.get_mut(ent.id_extract(id_bits)); 
+		vds::Bitset32<Size> * ent_states = w->internal_bitset_lookup(bitset_handle);
+		u64 id = ent.id_extract(id_bits);
+		bool alive = ent_states->is_set(id);
+		bool valid = w->ent_valid(ent);
+
+		if (alive && valid)
+		{
+			//Note(zshoals Dec-12-2022): We already know that the selected ID will be in bounds
+			//as everything in World is templated to a fixed size
+			return comps.get_mut_unsafe(id);
+		}
+		else
+		{
+			return nullptr;
+		}
 	}
 
-	T * comp_get_mut_unsafe(Entity ent) { return comps.get_mut_unsafe(ent.id_extract(id_bits)); }
+	T const * comp_get_unchecked(Entity ent) { return comps.get_unsafe(ent.id_extract(id_bits)); }
+	T * comp_get_mut_unchecked(Entity ent) { return comps.get_mut_unsafe(ent.id_extract(id_bits)); }
+
+	T * comp_set_ent(World<Size> * w, Entity ent)
+	{
+		vds::Bitset32<Size> * ent_states = w->internal_bitset_lookup(bitset_handle);
+		u64 id = ent.id_extract(id_bits);
+		bool not_set_in_component = ent_states->is_unset(id);
+		bool valid = w->ent_valid(ent);
+
+		DEBUG_ENSURE_TRUE(not_set_in_component, "Entity was already created in this bitset; did you set this entity twice?");
+		DEBUG_ENSURE_TRUE(valid, "Entity was invalid while trying to set a component; did you use an expired entity?");
+
+		if (not_set_in_component && valid)
+		{
+			//Note(zshoals Dec-12-2022): We already know that the selected ID will be in bounds
+			//as everything in World is templated to a fixed size
+			ent_states->set(id);
+			return comps.get_mut_unsafe(id);
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+
+	T * comp_set_ent_unchecked(Entity ent) { return comps.get_mut_unsafe(ent.id_extract(id_bits)); }
 };
 
 }
