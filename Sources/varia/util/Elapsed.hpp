4c843cb75f82603f149146d6e49fdac579d09411
z <zshoals@gmail.com>
2022-12-20 17:22:04 -0600

    auto track time in a scoped block

--- Sources/varia/util/Elapsed.hpp
+++ Sources/varia/util/Elapsed.hpp
@@ -13,6 +13,16 @@ struct Elapsed
 	double m_end = 0.0;
 	bool expired = false;
 
+	Elapsed(void)
+	{
+		this->m_start = kinc_time();
+	}
+	~Elapsed()
+	{
+		this->m_end = kinc_time();
+		log_time();
+	}
+
 	void begin(void) 
 	{ 
 		DEBUG_ENSURE_FALSE(this->expired, "This Elapsed has already been used. Create another one.");
@@ -29,6 +39,7 @@ struct Elapsed
 	void reset(void) { this->expired = false; }
 	void log_time(void) { VARIA_LOG_FLOAT(this->dt()); }
 	void log_time_message(char const * str) { VARIA_QLOG("%s :: %f", str, this->dt()); }
+	void end_and_log(void) { end(); log_time(); }
 };
 
 }
