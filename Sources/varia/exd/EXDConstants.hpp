58df18105f4df03357880a355da4a79d058beed3
z <zshoals@gmail.com>
2022-12-22 06:23:08 -0600

    Note weird behavior regarding entity counts

--- Sources/varia/exd/EXDConstants.hpp
+++ Sources/varia/exd/EXDConstants.hpp
@@ -9,6 +9,8 @@ namespace exd
 namespace Constants
 {
 
+	//Note(zshoals Dec-22-2022):> Numbers greater than 65536 actually cause entity creation
+	//to be faster by a factor of 2 almost. Wtf?
 	constexpr size_t exd_max_entities = 8192;
 	constexpr size_t exd_max_components = ComponentIdentifiers_e::MAX_COUNT;
 	constexpr u8 exd_id_shift = EXDUtil::id_shift(exd_max_entities);
