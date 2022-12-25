fd14251bd84d3119a65269c0c2107102b0892d31
z <zshoals@gmail.com>
2022-12-24 20:52:15 -0600

    namespace it and add some memory constants

--- Sources/varia/util/Memory.hpp
+++ Sources/varia/util/Memory.hpp
@@ -3,8 +3,14 @@
 namespace Varia
 {
 
+namespace Memory
+{
+
 constexpr size_t kilobytes_to_bytes(size_t KB) { return KB * 1024; }
 constexpr size_t megabytes_to_bytes(size_t MB) { return MB * 1024 * 1024; }
 constexpr size_t gigabytes_to_bytes(size_t GB) { return GB * 1024 * 1024 * 1024; }
 
+constexpr size_t default_alignment = 16;
+
+}
 }
