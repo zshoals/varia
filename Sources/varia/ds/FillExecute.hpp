69cd9f26741194fc9005835a99e257b979e5e112
z <zshoals@gmail.com>
2022-12-30 19:26:20 -0600

    Add datastructure that auto-callsback on a full buffer

---
+++ Sources/varia/ds/FillExecute.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "StaticRingbuf.hpp"
+
+namespace vds
+{
+
+template <typename T, typename Context, int Size>
+struct FillExecuteContextual
+{
+	vds::StaticRingbuf<T, Size> buffer;
+	void (*callback)(FillExecuteContextual<T, Context, Size> * self,  Context * ctx);
+	Context ctx;
+
+	//Note(zshoals 12-30-2022):> I'm gonna be honest, I have no idea what types these are
+	//lazy way out...
+	auto begin(void) { return buffer.begin(); }
+	auto end(void) { return buffer.end(); }
+
+	void initialize(void (*cb)(FillExecuteContextual<T, Context, Size> * self, Context * ctx), Context context)
+	{
+		this->ctx = context;
+		this->callback = cb;
+		buffer.initialize();
+	}
+
+	void push(T element)
+	{
+		if (this->buffer.is_full())
+		{
+			callback(this, &this->ctx);
+			this->buffer.clear();
+		}
+
+		this->buffer.push_back(element);
+	}
+};
+
+template <typename T, int Size>
+struct FillExecute
+{
+	vds::StaticRingbuf<T, Size> buffer;
+	void (*callback)(vds::FillExecute<T, Size> * self);
+
+	//Note(zshoals 12-30-2022):> I'm gonna be honest, I have no idea what types these are
+	//lazy way out...
+	auto begin(void) { return buffer.begin(); }
+	auto end(void) { return buffer.end(); }
+
+	void initialize(void (*cb)(vds::FillExecute<T, Size> * self))
+	{
+		this->callback = cb;
+		buffer.initialize();
+	}
+
+	void push(T element)
+	{
+		if (this->buffer.is_full())
+		{
+			callback(this);
+			this->buffer.clear();
+		}
+
+		this->buffer.push_back(element);
+	}
+};
+
+}
