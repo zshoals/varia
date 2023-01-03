3b6688ed914a5f098dd058208a4419a42db4a877
z <zshoals@gmail.com>
2023-01-03 16:22:44 -0600

    size in (kilo)bytes vs (kibi)bytes

--- Sources/varia/util/Memory.hpp
+++ Sources/varia/util/Memory.hpp
@@ -6,9 +6,9 @@ namespace Varia
 namespace Memory
 {
 
-constexpr size_t kilobytes_to_bytes(size_t KB) { return KB * 1024; }
-constexpr size_t megabytes_to_bytes(size_t MB) { return MB * 1024 * 1024; }
-constexpr size_t gigabytes_to_bytes(size_t GB) { return GB * 1024 * 1024 * 1024; }
+constexpr size_t kilobytes_to_bytes(size_t KB) { return KB * 1000; }
+constexpr size_t megabytes_to_bytes(size_t MB) { return MB * 1000 * 1000; }
+constexpr size_t gigabytes_to_bytes(size_t GB) { return GB * 1000 * 1000 * 1000; }
 
 constexpr size_t default_alignment = 16;
 
