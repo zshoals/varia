5eeaa9cc2d8c22f0597800397fddc21e899f70ba
z <zshoals@gmail.com>
2023-01-28 15:45:46 -0600

    Mostly convert entity over to free functions
    This ignores the underlying Bits data structure for the time being, which hasn't been converted

--- Sources/varia/exd/Entity.hpp
+++ Sources/varia/exd/Entity.hpp
@@ -4,57 +4,68 @@
 #include "varia/exd/EXDConstants.hpp"
 #include "varia/ds/Bits.hpp"
 
-namespace exd
-{
 
-struct Entity
+struct exd_entity_t
 {
 	u64 id;
-
-	Entity(u64 value) : id{value} {}
-	Entity() : id{0} {}
-
-	inline void generation_increment(void)
-	{
-		vds::Bits64 bitmode = {};
-		bitmode.storage = this->id;
-		bitmode.increment_upper(Constants::exd_id_shift);
-		this->id = bitmode.storage;
-	}
-
-	inline u64 generation_extract(void)
-	{
-		vds::Bits64 bitmode = {};
-		bitmode.storage = this->id;
-		bitmode.rotate_right(Constants::exd_id_shift);
-
-		return bitmode.storage;
-	}
-
-	inline u64 id_extract(void)
-	{
-		vds::Bits64 bitmode = {};
-		bitmode.storage = this->id;
-
-		vds::Bits64 lo_mask = {};
-		lo_mask.generate_bitmask_lo(Constants::exd_id_shift);
-
-		bitmode.mask_allow(lo_mask);
-
-		return bitmode.storage;
-	}
-
-	inline bool matches(Entity other) { return this->id == other.id; }
-
-	inline bool matches_or_exceeds(Entity other)
-	{
-		bool genOK = this->generation_extract() >= other.generation_extract();
-		bool idOK = this->id_extract() == other.id_extract();
-
-		return (genOK && idOK);
-	}
 };
 
-static Entity const INVALID_ENTITY = {SIZE_MAX};
+//TODO(zshoals 01-28-2023):> Uh oh...where were these used in the codebase?
+// Entity(u64 value) : id{value} {}
+// Entity() : id{0} {}
 
+
+
+
+
+//TODO(zshoals 01-28-2023):> Needs massive rework...requires pointers everywhere or return values
+//TODO(zshoals 01-28-2023):> Rename all "this" equivalent parameters to "self"
+inline void exd_entity_generation_increment(exd_entity_t * ent)
+{
+	//TODO(zshoals 01-28-2023):> God damnit, bits need reworked too :/
+	vds::Bits64 bitmode = {};
+	bitmode.storage = ent->id;
+	bitmode.increment_upper(exd::Constants::exd_id_shift);
+	ent->id = bitmode.storage;
+}
+
+inline u64 exd_entity_generation_extract(exd_entity_t ent)
+{
+	vds::Bits64 bitmode = {};
+	bitmode.storage = ent.id;
+	bitmode.rotate_right(exd::Constants::exd_id_shift);
+
+	return bitmode.storage;
+}
+
+inline u64 exd_entity_id_extract(exd_entity_t ent)
+{
+	vds::Bits64 bitmode = {};
+	bitmode.storage = ent.id;
+
+	vds::Bits64 lo_mask = {};
+	lo_mask.generate_bitmask_lo(exd::Constants::exd_id_shift);
+
+	bitmode.mask_allow(lo_mask);
+
+	return bitmode.storage;
+}
+
+inline bool exd_entity_matches(exd_entity_t a, exd_entity_t b) 
+{ 
+	return a.id == b.id;
+}
+
+inline bool exd_entity_matches_or_exceeds(exd_entity_t a, exd_entity_t b)
+{
+	bool genOK = ( exd_entity_generation_extract(a) >= exd_entity_generation_extract(b) );
+	bool idOK = ( exd_entity_id_extract(a) == exd_entity_id_extract(b) );
+
+	return (genOK && idOK);
+}
+
+//TODO(zshoals 01-28-2023):> Temporary invalid entity location to keep things working
+namespace exd
+{
+	static exd_entity_t const INVALID_ENTITY = {SIZE_MAX};
 }
