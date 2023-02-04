a5cf21baa5222b8ae6cceb2c520f8eb7d8d3c0e1
z <zshoals@gmail.com>
2023-02-03 19:02:02 -0600

    don't bake in assumptions from other modules

--- Sources/varia/io/File.cpp
+++ Sources/varia/io/File.cpp
@@ -3,10 +3,10 @@
 #include "varia/ds/Allocator.hpp"
 #include "kinc/io/filereader.h"
 
-vds_result_t<varia_io_file_t> varia_io_file_load_asset(const char * filepath, vds_allocator_t * allocator)
+static inline vds_result_t<varia_io_file_t> vds_io_file_unitlocal_load_file(const char * filepath, vds_allocator_t * allocator, int filetype)
 {
 	kinc_file_reader_t reader = {};
-	bool opened = kinc_file_reader_open(&reader, filepath, KINC_FILE_TYPE_ASSET);
+	bool opened = kinc_file_reader_open(&reader, filepath, filetype);
 
 	varia_io_file_t file;
 	{
@@ -20,15 +20,13 @@ vds_result_t<varia_io_file_t> varia_io_file_load_asset(const char * filepath, vd
 	if (opened)
 	{
 
-		size_t size = kinc_file_reader_size(&reader); //Null terminator
-		file.bytes = vds_allocator_malloc(allocator, unsigned char, size + 1);
+		size_t size = kinc_file_reader_size(&reader);
+		file.bytes = vds_allocator_malloc(allocator, unsigned char, size);
 
 		int size_read = kinc_file_reader_read(&reader, file.bytes, size);
-		++size_read;
 
 		file.size = size_read;
 		file.loaded = true;
-		file.bytes[size_read] = '\0';
 
 		res.valid = VDS_RESULT_STATUS_SUCCESS_E;
 
@@ -42,43 +40,12 @@ vds_result_t<varia_io_file_t> varia_io_file_load_asset(const char * filepath, vd
 	return res;
 }
 
+vds_result_t<varia_io_file_t> varia_io_file_load_asset(const char * filepath, vds_allocator_t * allocator)
+{
+	return vds_io_file_unitlocal_load_file(filepath, allocator, KINC_FILE_TYPE_ASSET);
+}
+
 vds_result_t<varia_io_file_t> varia_io_file_load_save(const char * filepath, vds_allocator_t * allocator)
 {
-	kinc_file_reader_t reader = {};
-	bool opened = kinc_file_reader_open(&reader, filepath, KINC_FILE_TYPE_SAVE);
-
-	varia_io_file_t file;
-	{
-		file.bytes = nullptr;
-		file.size = 0;
-		file.loaded = false;
-	}
-
-	vds_result_t<varia_io_file_t> res;
-
-	if (opened)
-	{
-		size_t size = kinc_file_reader_size(&reader); 
-		file.bytes = vds_allocator_malloc(allocator, unsigned char, size + 1); //Add a space for a null term
-
-		//Note(zshoals 02-03-2023):> This size_read value is one less byte than we allocated
-		//we allocate an extra byte for the null terminator
-		int size_read = kinc_file_reader_read(&reader, file.bytes, size);
-		++size_read; //Add the null terminator to the size
-
-		file.size = size_read;
-		file.loaded = true;
-		file.bytes[size_read] = '\0';
-
-		res.valid = VDS_RESULT_STATUS_SUCCESS_E;
-
-	}
-	else
-	{
-		res.valid = VDS_RESULT_STATUS_FAILURE_E;
-	}
-
-	res.value = file;
-	return res;
-
+	return vds_io_file_unitlocal_load_file(filepath, allocator, KINC_FILE_TYPE_SAVE);
 }
--- Sources/varia/io/File.hpp
+++ Sources/varia/io/File.hpp
@@ -14,18 +14,3 @@ struct varia_io_file_t
 
 vds_result_t<varia_io_file_t> varia_io_file_load_asset(const char * filepath, vds_allocator_t * allocator);
 vds_result_t<varia_io_file_t> varia_io_file_load_save(const char * filepath, vds_allocator_t * allocator);
-
-template<int MaxSize>
-vds_strview_sequence_t<MaxSize> varia_io_file_read_lines(varia_io_file_t file)
-{
-	char const * text = (char const *)(file.bytes);
-
-	char proxy[1];
-	proxy[0] = '\n';
-	char const * splitter = &proxy[0];
-
-	//TODO(zshoals 02-02-2023):> Strings from the config file aren't actually null terminated
-	//so that doesn't work, we need to count characters
-	vds_strview_sequence_t<MaxSize> dummy = vds_strview_split_by_v2<MaxSize>(vds_strview_create_with_length(text, file.size), splitter);
-	return dummy;
-}
