f0a5d5016c30db1e2ffd549915fce2d841d25441
z <zshoals@gmail.com>
2023-03-03 15:20:17 -0600

    split out SIMD types from their functions

---
+++ Sources/varia/simd/simd_types.h
@@ -0,0 +1,11 @@
+#include "kinc/simd/types.h"
+
+typedef kinc_float32x4_t f32q;
+typedef kinc_float32x4_mask_t f32q_mask;
+typedef f32q simd_fq;
+
+typedef kinc_int32x4_t i32q;
+typedef kinc_int32x4_mask_t i32q_mask;
+
+typedef kinc_uint32x4_t u32q;
+typedef kinc_uint32x4_mask_t u32q_mask;
