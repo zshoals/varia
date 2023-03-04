f8cd004197603b017251798ab686d59e056a9e1d
z <zshoals@gmail.com>
2023-03-04 14:51:18 -0600

    floor, ceil, round, etc

--- Sources/varia/simd/f32q.hpp
+++ Sources/varia/simd/f32q.hpp
@@ -24,6 +24,10 @@ VARIA_INLINE f32q f32q_neg(f32q n) { return kinc_float32x4_neg(n); }
 VARIA_INLINE f32q f32q_sqrt(f32q n) { return kinc_float32x4_sqrt(n); }
 VARIA_INLINE f32q f32q_reciprocal_approximation(f32q n) { return kinc_float32x4_reciprocal_approximation(n); }
 VARIA_INLINE f32q f32q_reciprocal_sqrt_approximation(f32q n) { return kinc_float32x4_reciprocal_sqrt_approximation(n); }
+VARIA_INLINE f32q f32q_and(f32q a, f32q b) { return kinc_float32x4_and(a, b); }
+VARIA_INLINE f32q f32q_or(f32q a, f32q b) { return kinc_float32x4_or(a, b); }
+VARIA_INLINE f32q f32q_xor(f32q a, f32q b) { return kinc_float32x4_xor(a, b); }
+VARIA_INLINE f32q f32q_not(f32q n) { return kinc_float32x4_not(n); }
 VARIA_INLINE f32q_mask f32q_cmpeq(f32q a, f32q b) { return kinc_float32x4_cmpeq(a, b); }
 VARIA_INLINE f32q_mask f32q_cmpneq(f32q a, f32q b) { return kinc_float32x4_cmpneq(a, b); }
 VARIA_INLINE f32q_mask f32q_cmpgt(f32q a, f32q b) { return kinc_float32x4_cmpgt(a, b); }
@@ -42,6 +46,13 @@ VARIA_INLINE void operator+=(f32q & a, f32q const & b) { a = f32q_add(a, b); }
 VARIA_INLINE void operator-=(f32q & a, f32q const & b) { a = f32q_sub(a, b); }
 VARIA_INLINE void operator*=(f32q & a, f32q const & b) { a = f32q_mul(a, b); }
 VARIA_INLINE void operator/=(f32q & a, f32q const & b) { a = f32q_div(a, b); }
+VARIA_INLINE f32q operator&(f32q const & a, f32q const & b) { return f32q_and(a, b); }
+VARIA_INLINE f32q operator|(f32q const & a, f32q const & b) { return f32q_or(a, b); }
+VARIA_INLINE f32q operator^(f32q const & a, f32q const & b) { return f32q_xor(a, b); }
+VARIA_INLINE f32q operator~(f32q const & n) { return f32q_not(n); }
+VARIA_INLINE void operator&=(f32q & a, f32q const & b) { a = f32q_and(a, b); }
+VARIA_INLINE void operator|=(f32q & a, f32q const & b) { a = f32q_or(a, b); }
+VARIA_INLINE void operator^=(f32q & a, f32q const & b) { a = f32q_xor(a, b); }
 VARIA_INLINE f32q_mask operator>(f32q const & a, f32q const & b) { return f32q_cmpgt(a, b); }
 VARIA_INLINE f32q_mask operator>=(f32q const & a, f32q const & b) { return f32q_cmpgte(a, b); }
 VARIA_INLINE f32q_mask operator<(f32q const & a, f32q const & b) { return f32q_cmplt(a, b); }
@@ -70,7 +81,85 @@ VARIA_INLINE f32q_mask f32q_mask_ones(void)
 	return f32q_cmpeq(zeroes, zeroes); 
 }
 
+VARIA_INLINE f32q f32q_round(f32q n)
+{
+	#if defined(KINC_SSE2)
+		__m128i y1 = _mm_cvtps_epi32(n);
+		f32q y2 = _mm_cvtepi32_ps(y1);
 
+		y2 |= (n & f32q_set_all(-0.0f));
+
+		f32q overflow_mask = _mm_cmpneq_ps(_mm_castsi128_ps(y1), _mm_castsi128_ps(_mm_set1_epi32(0x80000000)));
+
+		return f32q_select(overflow_mask, y2, n);
+	#else
+		n.values[0] = roundf(n.values[0]);
+		n.values[1] = roundf(n.values[1]);
+		n.values[2] = roundf(n.values[2]);
+		n.values[3] = roundf(n.values[3]);
+
+		return n;
+	#endif
+}
+
+VARIA_INLINE f32q f32q_truncate(f32q n)
+{
+	#if defined(KINC_SSE2)
+		__m128i y1 = _mm_cvttps_epi32(n);
+		f32q y2 = _mm_cvtepi32_ps(y1);
+
+		y2 |= (n & f32q_set_all(-0.0f));
+
+		f32q overflow_mask = _mm_cmpneq_ps(_mm_castsi128_ps(y1), _mm_castsi128_ps(_mm_set1_epi32(0x80000000)));
+
+		return f32q_select(overflow_mask, y2, n);
+	#else
+		n.values[0] = (float)((int)(n.values[0]));
+		n.values[1] = (float)((int)(n.values[1]));
+		n.values[2] = (float)((int)(n.values[2]));
+		n.values[3] = (float)((int)(n.values[3]));
+
+		return n;
+	#endif
+}
+
+VARIA_INLINE f32q f32q_floor(f32q n)
+{
+	#if defined(KINC_SSE) || defined(KINC_SSE2)
+		f32q y = f32q_round(n);
+		y += f32q_set_all(1.0f) & (y > n);
+
+		y |= (n & f32q_set_all(-0.0f));
+
+		return y;
+	#else
+		n.values[0] = floorf(n.values[0]);
+		n.values[1] = floorf(n.values[1]);
+		n.values[2] = floorf(n.values[2]);
+		n.values[3] = floorf(n.values[3]);
+
+		return n;
+	#endif
+}
+
+VARIA_INLINE f32q f32q_floor(f32q n)
+{
+	#if defined(KINC_SSE) || defined(KINC_SSE2)
+		f32q y = f32q_round(n);
+		y += f32q_set_all(1.0f) & (y < n);
+
+		y |= (n & f32q_set_all(-0.0f));
+
+		return y;
+	#else
+		n.values[0] = ceilf(n.values[0]);
+		n.values[1] = ceilf(n.values[1]);
+		n.values[2] = ceilf(n.values[2]);
+		n.values[3] = ceilf(n.values[3]);
+
+		return n;
+	#endif
+}
 
 VARIA_INLINE f32q f32q_pow(f32q n, int exp)
 {
@@ -145,4 +234,26 @@ VARIA_INLINE f32q f32q_tan(f32q angles)
 VARIA_INLINE f32q f32q_atan2(f32q ys, f32q xs)
 {
 	return simd_fq_atan2(ys, xs);
+}
+
+VARIA_INLINE f32q f32q_wrap_angle(f32q degrees)
+{
+	//355 + 1000
+	//1000 - 355 = 645
+
+	// 200 - 1000
+	//-800
+	f32q degrees_original = degrees;
+	f32q f360 = f32q_set_all(360.0f);
+	f32q f0 = f32q_zero();
+
+	//Surpasses 360 degrees
+	f32q ones = f32q_set_all(1.0f);
+	f32q angle_fill_percent = degrees / f360;
+
+	f32q underflows = degrees < f0;
+	f32q overflows = degrees >= f360;
+	f32q optional_add_one_if_underflow = angle_fill_percent * (underflows & ones);
+	f32q optional_sub_one_if_overflow = angle_fill_percent * (overflows & ones);
+
 }
