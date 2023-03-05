e01f348c3c4ad005c0cc45b55e2820a6f11d9341
z <zshoals@gmail.com>
2023-03-04 19:06:45 -0600

    suppress warnings

--- Sources/varia/math/MathConstants.hpp
+++ Sources/varia/math/MathConstants.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-constexpr double VARIA_EPSILON = 0.00000001;
-constexpr double VARIA_PI = 3.145926545;
-constexpr double VARIA_TAU = VARIA_PI * 2;
+constexpr double VARIA_EPSILON = 0.00000001f;
+constexpr double VARIA_PI = 3.145926545f;
+constexpr double VARIA_TAU = (VARIA_PI * 2.0f);
