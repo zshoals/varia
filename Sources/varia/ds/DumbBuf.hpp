8e1f6d8e626e7f4e850bde1aa07a0228895596b9
z <zshoals@gmail.com>
2023-03-08 11:45:35 -0600

    make sure the user doesn't write more elements than permitted

--- Sources/varia/ds/DumbBuf.hpp
+++ Sources/varia/ds/DumbBuf.hpp
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <string.h>
+#include <assert.h>
 
 template <typename T, int Size>
 struct alignas(64) vds_dumbbuf_t 
@@ -42,6 +43,12 @@ T * vds_dumbbuf_data(vds_dumbbuf_t<T, Size> * buf)
 }
 
 template <typename T, int Size>
+T * vds_dumbbuf_data_at_offset(vds_dumbbuf_t<T, Size> * buf)
+{
+	return &buf->buffer[buf->current_offset];
+}
+
+template <typename T, int Size>
 bool vds_dumbbuf_can_push_amount(vds_dumbbuf_t<T, Size> * buf, int64_t count)
 {
 	return (buf->current_offset + count) < Size;
@@ -63,15 +70,21 @@ bool vds_dumbbuf_try_push(vds_dumbbuf_t<T, Size> * buf, T element)
 	return false;
 }
 
-//Attempts to push --count-- values
-//Nothing gets pushed if pushing would overrun the buffer.
-template <typename T, int Size>
-bool vds_dumbbuf_safe_blit(vds_dumbbuf_t<T, Size> * buf, T * elements, int64_t count)
+//Verifies if --count-- values would fit inside the buffer.
+//If --count-- values fit, executes lambda F, passing along the current location in the buffer
+//Takes a dumbbuf, an element count, and a lambda that returns the actual amount of items written,
+//which should be equal to or less than --count--.
+//Returns true if the blit was successful, otherwise false.
+template <typename T, typename FUNC, int Size>
+bool vds_dumbbuf_try_blit(vds_dumbbuf_t<T, Size> * buf, int64_t count, FUNC f)
 {
-	if (vds_dumbbuf_can_push_amount(count))
+	if (vds_dumbbuf_can_push_amount(buf, count))
 	{
-		memcpy(buf->buffer[buf->current_offset], elements, sizeof(T) * count);
-		buf->current_offset += count;
+		int64_t written = f(&buf->buffer[buf->current_offset]);
+
+		assert(written <= count);
+
+		buf->current_offset += written;
 
 		return true;
 	}
