c9d8f7d080a4d0cbdb5859fb050612d1cc2b17f9
z <zshoals@gmail.com>
2023-03-08 15:00:57 -0600

    think about if this thing is useful

--- Sources/varia/ds/DumbBuf.hpp
+++ Sources/varia/ds/DumbBuf.hpp
@@ -4,87 +4,70 @@
 #include <string.h>
 #include <assert.h>
 
-template <typename T, int Size>
-struct alignas(64) vds_dumbbuf_t 
+#include "varia/ds/Allocator.hpp"
+
+template <typename T>
+struct vds_dumbbuf_t 
 {
-	T buffer[Size];
+	T * buffer;
+	int64_t _capacity;
 	int64_t current_offset;
 };
 
-template <typename T, int Size>
-void vds_dumbbuf_initialize(vds_dumbbuf_t<T, Size> * buf)
+template <typename T>
+void vds_dumbbuf_initialize(vds_dumbbuf_t<T> * buf, vds_allocator_t * alloc, size_t count)
 {
-	memset(buf->buffer[0], 0, sizeof(*buf));
+	buf->_capacity = count;
+	buf->current_offset = 0;
+	buf->buffer = vds_allocator_malloc_aligned(alloc, T, count, 64);
+	memset(&buf->buffer[0], 0, sizeof(T) * count)
 }
 
-
-template <typename T, int Size>
-int64_t vds_dumbbuf_reset(vds_dumbbuf_t<T, Size> * buf)
+template <typename T>
+int64_t vds_dumbbuf_reset(vds_dumbbuf_t<T> * buf)
 {
 	buf->current_offset = 0;
 }
 
-template <typename T, int Size>
-int64_t vds_dumbbuf_capacity(vds_dumbbuf_t<T, Size> * buf)
+template <typename T>
+int64_t vds_dumbbuf_capacity(vds_dumbbuf_t<T> * buf)
 {
-	return Size;
+	return buf->_capacity;
 }
 
-template <typename T, int Size>
-int64_t vds_dumbbuf_length(vds_dumbbuf_t<T, Size> * buf)
+template <typename T>
+int64_t vds_dumbbuf_length(vds_dumbbuf_t<T> * buf)
 {
 	return buf->current_offset;
 }
 
-template <typename T, int Size>
-T * vds_dumbbuf_data(vds_dumbbuf_t<T, Size> * buf)
+template <typename T>
+T * vds_dumbbuf_data(vds_dumbbuf_t<T> * buf)
 {
 	return &buf->buffer[0];
 }
 
-template <typename T, int Size>
-T * vds_dumbbuf_data_at_offset(vds_dumbbuf_t<T, Size> * buf)
+template <typename T>
+T * vds_dumbbuf_data_at_offset(vds_dumbbuf_t<T> * buf)
 {
 	return &buf->buffer[buf->current_offset];
 }
 
-template <typename T, int Size>
-bool vds_dumbbuf_can_push_amount(vds_dumbbuf_t<T, Size> * buf, int64_t count)
+template <typename T>
+bool vds_dumbbuf_can_push_amount(vds_dumbbuf_t<T> * buf, int64_t count)
 {
-	return (buf->current_offset + count) < Size;
+	return (buf->current_offset + count) < buf->_capacity;
 }
 
 //Attempts to push a value
 //Nothing gets pushed if pushing would overrun the buffer.
-template <typename T, int Size>
-bool vds_dumbbuf_try_push(vds_dumbbuf_t<T, Size> * buf, T element)
+template <typename T>
+bool vds_dumbbuf_try_push(vds_dumbbuf_t<T> * buf, T element)
 {
 	if (vds_dumbbuf_can_push_amount(buf, 1))
 	{
 		buf->buffer[buf->current_offset] = element;
 		++buf->current_offset;
-
-		return true;
-	}
-
-	return false;
-}
-
-//Verifies if --count-- values would fit inside the buffer.
-//If --count-- values fit, executes lambda F, passing along the current location in the buffer
-//Takes a dumbbuf, an element count, and a lambda that returns the actual amount of items written,
-//which should be equal to or less than --count--.
-//Returns true if the blit was successful, otherwise false.
-template <typename T, typename FUNC, int Size>
-bool vds_dumbbuf_try_blit(vds_dumbbuf_t<T, Size> * buf, int64_t count, FUNC f)
-{
-	if (vds_dumbbuf_can_push_amount(buf, count))
-	{
-		int64_t written = f(&buf->buffer[buf->current_offset]);
-
-		assert(written <= count);
-
-		buf->current_offset += written;
 
 		return true;
 	}
