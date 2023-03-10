13a82b0e8f00a8067fea0fb9b95aa467338368f4
z <zshoals@gmail.com>
2023-03-10 16:19:13 -0600

    add int32 and float32 rounding and truncation

--- Sources/varia/simd/impl/type_conversions.h
+++ Sources/varia/simd/impl/type_conversions.h
@@ -15,6 +15,34 @@ extern "C" {
 
 #if defined(KINC_SSE2)
 
+//====================================================
+//                  Conversion
+//====================================================
+
+
+
+static inline varia_int32x4_t varia_float32x4_convert_to_int32x4(varia_float32x4_t t)
+{
+	return _mm_cvtps_epi32(t);
+}
+
+static inline varia_int32x4_t varia_float32x4_truncate_to_int32x4(varia_float32x4_t t)
+{
+	return _mm_cvttps_epi32(t);
+}
+
+static inline varia_float32x4_t varia_int32x4_convert_to_float32x4(varia_int32x4_t t)
+{
+	return _mm_cvtepi32_ps(t);
+}
+
+
+
+//====================================================
+//                  Casts
+//====================================================
+
+
 
 //Float32x4 ----> Other
 static inline varia_int32x4_t varia_float32x4_cast_to_int32x4(varia_float32x4_t t) {
@@ -51,6 +79,67 @@ static inline varia_int32x4_t varia_uint32x4_cast_to_int32x4(varia_uint32x4_t t)
 
 
 #elif defined(KINC_SSE)
+
+//====================================================
+//                  Conversion
+//====================================================
+
+#include <math.h>
+
+
+static inline varia_int32x4_t varia_float32x4_convert_to_int32x4(varia_float32x4_t t)
+{
+	float extracted[4];
+	_mm_storeu_ps(&extracted[0], t);
+
+	varia_int32x4_t cvt;
+
+	cvt.values[0] = (int32_t)roundf(extracted[0]);
+	cvt.values[1] = (int32_t)roundf(extracted[1]);
+	cvt.values[2] = (int32_t)roundf(extracted[2]);
+	cvt.values[3] = (int32_t)roundf(extracted[3]);
+
+	return cvt;
+}
+
+static inline varia_int32x4_t varia_float32x4_truncate_to_int32x4(varia_float32x4_t t)
+{
+	//FILL ME
+	float extracted[4];
+	_mm_storeu_ps(&extracted[0], t);
+
+	varia_int32x4_t cvt;
+
+	cvt.values[0] = (int32_t)truncf(extracted[0]);
+	cvt.values[1] = (int32_t)truncf(extracted[1]);
+	cvt.values[2] = (int32_t)truncf(extracted[2]);
+	cvt.values[3] = (int32_t)truncf(extracted[3]);
+
+	return cvt;
+}
+
+static inline varia_float32x4_t varia_int32x4_convert_to_float32x4(varia_int32x4_t t)
+{
+	float cvt[4];
+
+	cvt[0] = (float)t.values[0];
+	cvt[1] = (float)t.values[1];
+	cvt[2] = (float)t.values[2];
+	cvt[3] = (float)t.values[3];
+
+	return _mm_loadu_ps(&cvt[0]);
+}
+
+
+
+
+
+
+//====================================================
+//                  Casts
+//====================================================
+
+
 
 
 //Float32x4 ----> Other
@@ -97,6 +186,35 @@ static inline varia_float32x4_t varia_uint32x4_cast_to_float32x4(varia_uint32x4_
 
 #elif defined(KINC_NEON)
 
+//====================================================
+//                  Conversion
+//====================================================
+
+
+static inline varia_int32x4_t varia_float32x4_convert_to_int32x4(varia_float32x4_t t)
+{
+	return vcvtaq_s32_f32(t);
+}
+
+static inline varia_int32x4_t varia_float32x4_truncate_to_int32x4(varia_float32x4_t t)
+{
+	return vcvtq_s32_f32(t);
+}
+
+static inline varia_float32x4_t varia_int32x4_convert_to_float32x4(varia_int32x4_t t)
+{
+	return vcvtq_f32_s32(t);
+}
+
+
+
+
+//====================================================
+//                  Casts
+//====================================================
+
+
+
 
 //Float32x4 ----> Other
 static inline varia_int32x4_t varia_float32x4_cast_to_int32x4(varia_float32x4_t t) {
@@ -130,6 +248,59 @@ static inline varia_int32x4_t varia_uint32x4_cast_to_int32x4(varia_uint32x4_t t)
 
 //KINC_NOSIMD float fallbacks casts
 #else
+
+
+
+
+//====================================================
+//                  Conversion
+//====================================================
+
+#include <math.h>
+
+static inline varia_int32x4_t varia_float32x4_convert_to_int32x4(varia_float32x4_t t)
+{
+	varia_int32x4_t out;
+
+	out.values[0] = (int32_t)roundf(t.values[0]);
+	out.values[1] = (int32_t)roundf(t.values[1]);
+	out.values[2] = (int32_t)roundf(t.values[2]);
+	out.values[3] = (int32_t)roundf(t.values[3]);
+
+	return out;
+}
+
+static inline varia_int32x4_t varia_float32x4_truncate_to_int32x4(varia_float32x4_t t)
+{
+	varia_int32x4_t out;
+
+	out.values[0] = (int32_t)truncf(t.values[0]);
+	out.values[1] = (int32_t)truncf(t.values[1]);
+	out.values[2] = (int32_t)truncf(t.values[2]);
+	out.values[3] = (int32_t)truncf(t.values[3]);
+
+	return out;
+}
+
+static inline varia_float32x4_t varia_int32x4_convert_to_float32x4(varia_int32x4_t t)
+{
+	varia_float32x4_t out;
+
+	out.values[0] = (float)t.values[0];
+	out.values[1] = (float)t.values[1];
+	out.values[2] = (float)t.values[2];
+	out.values[3] = (float)t.values[3];
+
+	return out;
+}
+
+
+
+
+//====================================================
+//                  Casts
+//====================================================
+
 
 
 
