7c90f4627e5bf1a6da81eb2d97a7c2c96eacead5
z <zshoals@gmail.com>
2023-03-10 14:59:13 -0600

    simplify the debugging layer, like, a lot

--- Sources/varia/validation.cpp
+++ Sources/varia/validation.cpp
@@ -1,370 +1,49 @@
 #include "varia/validation.hpp"
 
 #include "kinc/log.h"
+#include "log.hpp"
 #include <math.h>
 
 #define VARIA_VALIDATION_NO_OP() ((void)0)
 #define VARIA_VALIDATION_FLOAT_EPSILON 0.00000001
 
 
-
-
-
 void varia_custom_assert(bool condition, char const * expr, char const * message, char const * file, int line)
 {
 	if (!condition)
 	{
+		Glog_print(); //Write any global unlogged info out
+		Glog_clear_buffer();
+
 		kinc_log(KINC_LOG_LEVEL_ERROR, "| Assertion failed.\n|   %s \n|   Message: %s \n|   File: %s \n|   Line: %d \n", expr, message, file, line);
 
-		__debugbreak();
+		#if defined(_MSC_VER)
+			__debugbreak();
+		#elif defined(__clang__)
+			__builtin_debugtrap();
+		#else
+		#if defined(__aarch64__)
+			__asm__ volatile(".inst 0xd4200000");
+		#elif defined(__x86_64__)
+			__asm__ volatile("int $0x03");
+		#else
+			kinc_log(KINC_LOG_LEVEL_WARNING, "Oh no, kinc_debug_break is not implemented for the current compiler and CPU.");
+			assert(false && "No debugger available: aborting...");
+		#endif
+		#endif
 	}
 }
 
 
-
-//======================================
-//=========INT CHECKS===================
-//======================================
-
-void varia_validation_ensure_int_gt(long long a, long long b, char const * message, char const * file, int line)
+void varia_custom_assert_release(bool condition, char const * expr, char const * message, char const * file, int line)
 {
-	if (a > b)
+	if (!condition)
 	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %lld greater than %lld", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a > b, 0) );
-	}
-}
-void varia_validation_ensure_int_gte(long long a, long long b, char const * message, char const * file, int line)
-{
-	if (a >= b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
+		Glog_print();
+		Glog_clear_buffer();
 
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %lld greater than or equal to %lld", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a >= b, 0) );
-	}
-}
-void varia_validation_ensure_int_lt(long long a, long long b, char const * message, char const * file, int line)
-{
-	if (a < b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
+		kinc_log(KINC_LOG_LEVEL_ERROR, "| Assertion failed.\n|   %s \n|   Message: %s \n|   File: %s \n|   Line: %d \n", expr, message, file, line);
 
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %lld less than %lld", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a < b, 0) );
-	}
-}
-void varia_validation_ensure_int_lte(long long a, long long b, char const * message, char const * file, int line)
-{
-	if (a <= b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %lld less than or equal to %lld", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a <= b, 0) );
-	}
-}
-void varia_validation_ensure_int_equals(long long a, long long b, char const * message, char const * file, int line)
-{
-	if (a == b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %lld equals %lld", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a == b, 0) );
-	}
-}
-void varia_validation_ensure_int_in_range(long long value, long long lower, long long upper, char const * message, char const * file, int line)
-{
-	if (lower <= value && value < upper)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %lld to be in range of lower range %lld and upper range %lld.", value, lower, upper);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, lower <= value && value < upper, 0) );
-	}
-}
-
-
-//================================
-//=====UNSIGNED INT CHECKS========
-//================================
-
-void varia_validation_ensure_uint_gt(unsigned long long a, unsigned long long b, char const * message, char const * file, int line)
-{
-	if (a > b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %llu greater than %llu", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a > b, 0) );
-	}
-}
-void varia_validation_ensure_uint_gte(unsigned long long a, unsigned long long b, char const * message, char const * file, int line)
-{
-	if (a >= b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %llu greater than or equal to %llu", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a >= b, 0) );
-	}
-}
-void varia_validation_ensure_uint_lt(unsigned long long a, unsigned long long b, char const * message, char const * file, int line)
-{
-	if (a < b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %llu less than %llu", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a < b, 0) );
-	}
-}
-void varia_validation_ensure_uint_lte(unsigned long long a, unsigned long long b, char const * message, char const * file, int line)
-{
-	if (a <= b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %llu less than or equal to %llu", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a <= b, 0) );
-	}
-}
-void varia_validation_ensure_uint_equals(unsigned long long a, unsigned long long b, char const * message, char const * file, int line)
-{
-	if (a == b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %llu equals %llu", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a == b, 0) );
-	}
-}
-void varia_validation_ensure_uint_in_range(unsigned long long value, unsigned long long lower, unsigned long long upper, char const * message, char const * file, int line)
-{
-	if (lower <= value && value < upper)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected value %llu to be in range of lower range %llu and upper range %llu", value, lower, upper);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, lower <= value && value < upper, 0) );
-	}
-}
-
-//======================================
-//=====UNSIGNED FLOAT/DOUBLE CHECKS=====
-//======================================
-
-void varia_validation_ensure_float_gt(long double a, long double b, char const * message, char const * file, int line)
-{
-	if (a > b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %Lf greater than %Lf", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a > b, 0) );
-	}
-}
-void varia_validation_ensure_float_gte(long double a, long double b, char const * message, char const * file, int line)
-{
-	if (a >= b || fabs(a - b) < VARIA_VALIDATION_FLOAT_EPSILON)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %Lf greater than or equal to %Lf", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a >= b || fabs(a - b) < VARIA_VALIDATION_FLOAT_EPSILON, 0) );
-	}
-}
-void varia_validation_ensure_float_lt(long double a, long double b, char const * message, char const * file, int line)
-{
-	if (a < b)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %Lf less than %Lf", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a < b, 0) );
-	}
-}
-void varia_validation_ensure_float_lte(long double a, long double b, char const * message, char const * file, int line)
-{
-	if (a <= b || fabs(a - b) < VARIA_VALIDATION_FLOAT_EPSILON)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %Lf less than or equal to %Lf", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, a <= b || fabs(a - b) < VARIA_VALIDATION_FLOAT_EPSILON, 0) );
-	}
-}
-void varia_validation_ensure_float_equals(long double a, long double b, char const * message, char const * file, int line)
-{
-	if (fabs(a - b) < VARIA_VALIDATION_FLOAT_EPSILON)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %Lf equals %Lf", a, b);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, fabs(a - b) < VARIA_VALIDATION_FLOAT_EPSILON, 0) );
-	}
-}
-void varia_validation_ensure_float_in_range(long double value, long double lower, long double upper, char const * message, char const * file, int line)
-{
-	if ( (lower <= value || fabs(lower - value) < VARIA_VALIDATION_FLOAT_EPSILON) && value < upper)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Expected %Lf in the range of lower range %Lf and upper range %Lf", value, lower, upper);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, ((lower <= value || fabs(lower - value) < VARIA_VALIDATION_FLOAT_EPSILON) && value < upper), 0) );
-	}
-}
-
-//======================================
-//=====POINTER CHECKS===================
-//======================================
-
-void varia_validation_ensure_ptr_not_null(void * ptr, char const * message, char const * file, int line)
-{
-	if (ptr)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Pointer was null!");
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, ptr, 0) );
-	}
-}
-void varia_validation_ensure_ptr_is_null(void * ptr, char const * message, char const * file, int line)
-{
-	if (!ptr)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Pointer was not null!");
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, !ptr, 0) );
-	}
-}
-void varia_validation_ensure_const_ptr_not_null(void const * ptr, char const * message, char const * file, int line)
-{
-	if (ptr)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Const Pointer was null!");
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, ptr, 0) );
-	}
-}
-void varia_validation_ensure_const_ptr_is_null(void const * ptr, char const * message, char const * file, int line)
-{
-	if (!ptr)
-	{
-		VARIA_VALIDATION_NO_OP();
-	}
-	else
-	{
-		kinc_log(KINC_LOG_LEVEL_ERROR, "\nASSERTION FAILED::[FILE: %s]::[LINE: %d]", file, line);
-		kinc_log(KINC_LOG_LEVEL_ERROR, "Const Pointer was not null!");
-		kinc_log(KINC_LOG_LEVEL_ERROR, "User Message: %s", message);
-		assert( (message, !ptr, 0) );
+		assert(false && "A critical runtime error has occurred. Please contact the developer.");
 	}
 }
--- Sources/varia/validation.hpp
+++ Sources/varia/validation.hpp
@@ -8,148 +8,25 @@
 	#define NDEBUG
 #endif
 
-void varia_validation_ensure_int_gt(long long a, long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_int_gte(long long a, long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_int_lt(long long a, long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_int_lte(long long a, long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_int_equals(long long a, long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_int_in_range(long long value, long long lower, long long upper, char const * message, char const * file, int line);
 
-void varia_validation_ensure_uint_gt(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_uint_gte(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_uint_lt(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_uint_lte(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_uint_equals(unsigned long long a, unsigned long long b, char const * message, char const * file, int line);
-void varia_validation_ensure_uint_in_range(unsigned long long value, unsigned long long lower, unsigned long long upper, char const * message, char const * file, int line);
-
-void varia_validation_ensure_float_gt(long double a, long double b, char const * message, char const * file, int line);
-void varia_validation_ensure_float_gte(long double a, long double b, char const * message, char const * file, int line);
-void varia_validation_ensure_float_lt(long double a, long double b, char const * message, char const * file, int line);
-void varia_validation_ensure_float_lte(long double a, long double b, char const * message, char const * file, int line);
-void varia_validation_ensure_float_equals(long double a, long double b, char const * message, char const * file, int line);
-void varia_validation_ensure_float_in_range(long double value, long double lower, long double upper, char const * message, char const * file, int line);
-
-void varia_validation_ensure_ptr_not_null(void * ptr, char const * message, char const * file, int line);
-void varia_validation_ensure_ptr_is_null(void * ptr, char const * message, char const * file, int line);
-
-void varia_validation_ensure_const_ptr_not_null(void const * ptr, char const * message, char const * file, int line);
-void varia_validation_ensure_const_ptr_is_null(void const * ptr, char const * message, char const * file, int line);
 
 void varia_custom_assert(bool condition, char const * expr, char const * message, char const * file, int line);
+void varia_custom_assert_release(bool condition, char const * expr, char const * message, char const * file, int line);
 
 #ifndef VARIA_DISABLE_UNFORCED_ASSERTS
-
-	#define DEBUG_ASSERT(Expression, Message) varia_custom_assert(Expression, #Expression, Message, __FILE__, __LINE__)
-
-
-	#define DEBUG_ENSURE_TRUE(Expression, Message) assert(( (Message), (Expression) ))
-	#define DEBUG_ENSURE_FALSE(Expression, Message) assert(( (Message), !(Expression) ))
-	#define DEBUG_ENSURE_KILL_NOW(Message) assert(( (Message), 0 ))
-
-	#define DEBUG_ENSURE_INT_GT(A, B, Message) varia_validation_ensure_int_gt(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_INT_GTE(A, B, Message) varia_validation_ensure_int_gte(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_INT_LT(A, B, Message) varia_validation_ensure_int_lt(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_INT_LTE(A, B, Message) varia_validation_ensure_int_lte(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_INT_EQUALS(A, B, Message) varia_validation_ensure_int_equals(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_INT_IN_RANGE(Value, Lower, Upper, Message) varia_validation_ensure_int_in_range(Value, Lower, Upper, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_INT_GT_ZERO(Value, Message) varia_validation_ensure_int_gt(Value, 0, Message, __FILE__, __LINE__)
-
-	#define DEBUG_ENSURE_UINT_GT(A, B, Message) varia_validation_ensure_uint_gt(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_UINT_GTE(A, B, Message) varia_validation_ensure_uint_gte(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_UINT_LT(A, B, Message) varia_validation_ensure_uint_lt(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_UINT_LTE(A, B, Message) varia_validation_ensure_uint_lte(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_UINT_EQUALS(A, B, Message) varia_validation_ensure_uint_equals(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_UINT_IN_RANGE(Value, Lower, Upper, Message) varia_validation_ensure_uint_in_range(Value, Lower, Upper, Message,  __FILE__, __LINE__)
-	#define DEBUG_ENSURE_UINT_GT_ZERO(Value, Message) varia_validation_ensure_uint_gt(Value, 0, Message, __FILE__, __LINE__)
-
-	#define DEBUG_ENSURE_FLOAT_GT(A, B, Message) varia_validation_ensure_float_gt(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_FLOAT_GTE(A, B, Message) varia_validation_ensure_float_gte(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_FLOAT_LT(A, B, Message) varia_validation_ensure_float_lt(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_FLOAT_LTE(A, B, Message) varia_validation_ensure_float_lte(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_FLOAT_EQUALS(A, B, Message) varia_validation_ensure_float_equals(A, B, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_FLOAT_IN_RANGE(Value, Lower, Upper, Message) varia_validation_ensure_float_in_range(Value, Lower, Upper, Message,  __FILE__, __LINE__)
-
-	#define DEBUG_ENSURE_PTR_NOT_NULL(Pointer, Message) varia_validation_ensure_ptr_not_null(Pointer, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_PTR_IS_NULL(Pointer, Message) varia_validation_ensure_ptr_is_null(Pointer, Message, __FILE__, __LINE__)
-
-	#define DEBUG_ENSURE_CONST_PTR_NOT_NULL(Pointer, Message) varia_validation_ensure_const_ptr_not_null(Pointer, Message, __FILE__, __LINE__)
-	#define DEBUG_ENSURE_CONST_PTR_IS_NULL(Pointer, Message) varia_validation_ensure_const_ptr_is_null(Pointer, Message, __FILE__, __LINE__)
-
+	#define DEBUG_ENSURE(Expression, Message) varia_custom_assert(Expression, #Expression, Message, __FILE__, __LINE__)
+	#define DEBUG_ENSURE_KILL_NOW(Message) varia_custom_assert(false, "DEBUG ERROR", Message, __FILE__, __LINE__)
 #else
-	#define DEBUG_ENSURE_TRUE(Expression, Message) ((void)0)
-	#define DEBUG_ENSURE_FALSE(Expression, Message) ((void)0)
+	#define DEBUG_ENSURE(Expression, Message) ((void)0)
 	#define DEBUG_ENSURE_KILL_NOW(Message) ((void)0)
-
-	#define DEBUG_ENSURE_INT_GT(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_INT_GTE(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_INT_LT(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_INT_LTE(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_INT_EQUALS(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_INT_IN_RANGE(Value, Lower, Upper, Message) ((void)0)
-	#define DEBUG_ENSURE_INT_GT_ZERO(Value, Message) ((void)0)
-
-	#define DEBUG_ENSURE_UINT_GT(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_UINT_GTE(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_UINT_LT(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_UINT_LTE(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_UINT_EQUALS(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_UINT_IN_RANGE(Value, Lower, Upper, Message) ((void)0)
-	#define DEBUG_ENSURE_UINT_GT_ZERO(Value, Message) ((void)0)
-
-	#define DEBUG_ENSURE_FLOAT_GT(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_FLOAT_GTE(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_FLOAT_LT(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_FLOAT_LTE(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_FLOAT_EQUALS(A, B, Message) ((void)0)
-	#define DEBUG_ENSURE_FLOAT_IN_RANGE(Value, Lower, Upper, Message) ((void)0)
-
-	#define DEBUG_ENSURE_PTR_NOT_NULL(Pointer, Message) ((void)0)
-	#define DEBUG_ENSURE_PTR_IS_NULL(Pointer, Message) ((void)0)
-
-	#define DEBUG_ENSURE_CONST_PTR_NOT_NULL(Pointer, Message) ((void)0)
-	#define DEBUG_ENSURE_CONST_PTR_IS_NULL(Pointer, Message) ((void)0)
 #endif
 
 
-//These assertions are never disabled, even in release builds
-//An important note: These may need have a different assert mechanism, otherwise it may enter a debugmode state
-//in release to clients (not good!). Might have to change these to abort() or something instead,
-//maybe with some sort of crash message box.
-#define ENSURE_TRUE(Expression, Message) assert(( (Message), (Expression) ))
-#define ENSURE_FALSE(Expression, Message) assert(( (Message), !(Expression) ))
-
-#define ENSURE_INT_GT(A, B, Message) varia_validation_ensure_int_gt(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_INT_GTE(A, B, Message) varia_validation_ensure_int_gte(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_INT_LT(A, B, Message) varia_validation_ensure_int_lt(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_INT_LTE(A, B, Message) varia_validation_ensure_int_lte(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_INT_EQUALS(A, B, Message) varia_validation_ensure_int_equals(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_INT_IN_RANGE(Value, Lower, Upper, Message) varia_validation_ensure_int_in_range(Value, Lower, Upper, Message, __FILE__, __LINE__)
-#define ENSURE_INT_GT_ZERO(Value, Message) varia_validation_ensure_int_gt(Value, 0, Message, __FILE__, __LINE__)
-#define ENSURE_INT_GTE_ZERO(Value, Message) varia_validation_ensure_int_gte(Value, 0, Message, __FILE__, __LINE__)
-
-#define ENSURE_UINT_GT(A, B, Message) varia_validation_ensure_uint_gt(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_UINT_GTE(A, B, Message) varia_validation_ensure_uint_gte(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_UINT_LT(A, B, Message) varia_validation_ensure_uint_lt(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_UINT_LTE(A, B, Message) varia_validation_ensure_uint_lte(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_UINT_EQUALS(A, B, Message) varia_validation_ensure_uint_equals(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_UINT_IN_RANGE(Value, Lower, Upper, Message) varia_validation_ensure_uint_in_range(Value, Lower, Upper, Message,  __FILE__, __LINE__)
-#define ENSURE_UINT_GT_ZERO(Value, Message) varia_validation_ensure_uint_gt(Value, 0, Message, __FILE__, __LINE__)
-#define ENSURE_UINT_GTE_ZERO(Value, Message) varia_validation_ensure_uint_gte(Value, 0, Message, __FILE__, __LINE__)
-
-#define ENSURE_FLOAT_GT(A, B, Message) varia_validation_ensure_float_gt(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_FLOAT_GTE(A, B, Message) varia_validation_ensure_float_gte(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_FLOAT_LT(A, B, Message) varia_validation_ensure_float_lt(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_FLOAT_LTE(A, B, Message) varia_validation_ensure_float_lte(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_FLOAT_EQUALS(A, B, Message) varia_validation_ensure_float_equals(A, B, Message, __FILE__, __LINE__)
-#define ENSURE_FLOAT_IN_RANGE(Value, Lower, Upper, Message) varia_validation_ensure_float_in_range(Value, Lower, Upper, Message,  __FILE__, __LINE__)
-
-#define ENSURE_PTR_NOT_NULL(Pointer, Message) varia_validation_ensure_ptr_not_null(Pointer, Message, __FILE__, __LINE__)
-#define ENSURE_PTR_IS_NULL(Pointer, Message) varia_validation_ensure_ptr_is_null(Pointer, Message, __FILE__, __LINE__)
-
+#define ENSURE(Expression, Message) varia_custom_assert_release(Expression, #Expression, Message, __FILE__, __LINE__)
 //Something must die if you reach this point
-#define ENSURE_KILL_NOW(Message) assert(( (Message), 0 ))
+#define ENSURE_KILL_NOW(Message) assert(false && "RUNTIME ERROR: Emergency Kill Now in file: " __FILE__)
 //Intended for things like switch cases with unreachable and unwanted default outcomes, for instance
-#define ENSURE_UNREACHABLE(Message) assert(( (Message), 0 ))
+#define ENSURE_UNREACHABLE(Message) assert(false && "RUNTIME ERROR: Unreachable was reached in file: " __FILE__)
 //Intended for unimplemented functions and features, maybe we should add function name support
-#define ENSURE_UNIMPLEMENTED() assert(( "Function is unimplemented.", 0 ))
+#define ENSURE_UNIMPLEMENTED() assert(false && "RUNTIME ERROR: Function is unimplemented in file: " __FILE__)
 
