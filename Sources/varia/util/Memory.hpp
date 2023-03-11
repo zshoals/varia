d0401421a21c07da61857ea3e30fb128e8d1f4fd
z <zshoals@gmail.com>
2023-03-10 18:33:06 -0600

    we don't need to split headers that hard

--- Sources/varia/util/Memory.hpp
+++ Sources/varia/util/Memory.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "varia/vcommon.hpp"
+#include <string.h>
 
 constexpr size_t varia_memory_kilobytes_to_bytes(size_t KB) { return KB * 1000; }
 constexpr size_t varia_memory_megabytes_to_bytes(size_t MB) { return MB * 1000 * 1000; }
@@ -8,3 +9,6 @@ constexpr size_t varia_memory_gigabytes_to_bytes(size_t GB) { return GB * 1000 *
 
 constexpr size_t VARIA_MEMORY_CONSTANTS_DEFAULT_ALIGNMENT = 16;
 
+#define varia_bitcast(DESTINATION_PTR, SOURCE_PTR, SIZE)\
+	memcpy((DESTINATION_PTR), (SOURCE_PTR), (SIZE))
+	
