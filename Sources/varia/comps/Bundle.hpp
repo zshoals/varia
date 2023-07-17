2c0e3293904bb51b0a793dc1a2d40b9ca321ff61
zshoals <zshoals@gmail.com>
2023-07-17 07:27:50 -0500

    get rid of all of this stuff and start over

--- Sources/varia/comps/Bundle.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-struct Foo {char a;};
-struct Bar {float a; };
-struct Degrees { float deg; };
-struct Radians { float rad; };
--- Sources/varia/tests/Test_Everything.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Test_StringViews.hpp"
-
-void test_add_every_test_to_dread(void)
-{
-	test_strview_all_tests();
-}
