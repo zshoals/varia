f483162e1a19e0496d6b6eb01b52f2b919211339
zshoals <zshoals@gmail.com>
2023-07-30 15:20:33 -0500

    ditto

--- Sources/varia/VSimulation.cpp
+++ Sources/varia/VSimulation.cpp
@@ -2,5 +2,10 @@
 
 void v_simulation_simulate(Gamestate * gs, E_Simulation_Mode mode)
 {
+    if (mode == E_Simulation_Mode::Pause)
+    {
+        return;
+    }
+
     //TODO(<zshoals> 07-27-2023): Everything
 }
