d0336df887876ecdb62339ea9d1ab0b111d8c8d4
zshoals <zshoals@gmail.com>
2023-08-02 00:37:20 -0500

    improve array utility

--- Sources/varia/ds/VDS-Array.hpp
+++ Sources/varia/ds/VDS-Array.hpp
@@ -32,36 +32,6 @@ VDS_Array<T> vds_array_make_interface(VDS_Array_Storage<T, SIZE> * storage)
 }
 
 template <typename T>
-Boolean vds_array_can_push(VDS_Array<T> const * array)
-{
-    return ( *(array->push_head) < array->capacity );
-}
-
-template <typename T>
-void vds_array_push(VDS_Array<T> * array, T element)
-{
-    VDS_ASSERT( *(array->push_head) + 1 <= array->capacity, "Push would overrun array");
-
-    array->data[ *(array->push_head) ] = element;
-    *(array->push_head) += 1;
-}
-
-template <typename T>
-Boolean vds_array_can_pop(VDS_Array<T> const * array)
-{
-    return ( *(array->push_head) > 0);
-}
-
-template <typename T>
-T vds_array_pop(VDS_Array<T> * array)
-{
-    VDS_ASSERT( *(array->push_head) - 1 >= 0, "Pop would underrun array");
-
-    *(array->push_head) -= 1;
-    return *(array->data[ *(array->push_head) ]);
-}
-
-template <typename T>
 T vds_array_copy_of(VDS_Array<T> const * array, Integer_64 index)
 {
     return array->data[index];
@@ -77,6 +47,47 @@ template <typename T>
 T const * vds_array_const_location_of(VDS_Array<T> const * array, Integer_64 index)
 {
     return &(array->data[index]);
+}
+
+template <typename T>
+Boolean vds_array_can_push(VDS_Array<T> const * array)
+{
+    return ( *(array->push_head) < array->capacity );
+}
+
+template <typename T>
+void vds_array_push(VDS_Array<T> * array, T element)
+{
+    VDS_ASSERT( *(array->push_head) + 1 <= array->capacity, "Push would overrun array");
+
+    array->data[ *(array->push_head) ] = element;
+    *(array->push_head) += 1;
+}
+
+template <typename T>
+T * vds_array_construct_push(VDS_Array<T> * array)
+{
+    VDS_ASSERT( *(array->push_head) + 1 <= array->capacity, "Push would overrun array");
+
+    T * element = vds_array_location_of(array, *(array->push_head));
+    *(array->push_head) += 1;
+
+    return element;
+}
+
+template <typename T>
+Boolean vds_array_can_pop(VDS_Array<T> const * array)
+{
+    return ( *(array->push_head) > 0);
+}
+
+template <typename T>
+T vds_array_pop(VDS_Array<T> * array)
+{
+    VDS_ASSERT( *(array->push_head) - 1 >= 0, "Pop would underrun array");
+
+    *(array->push_head) -= 1;
+    return *(array->data[ *(array->push_head) ]);
 }
 
 template <typename T, typename FUNC>
