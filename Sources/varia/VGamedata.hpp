6457d861b1ddfe113fb5fce858f1f23f9f345248
zshoals <zshoals@gmail.com>
2023-08-09 05:20:33 -0500

    add prototype gamedata for testing

---
+++ Sources/varia/VGamedata.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "varia/ds/VDS-Array.hpp"
+#include "varia/ds/VDS-String.hpp"
+
+//NOTE(<zshoals> 08-09-2023): This struct houses the ACTUAL gameplay stuff, things like
+//  player and enemy behaviors, not info that's more meta than that. Meta info is in "Gamestate"
+struct Gamedata
+{
+    VDS_Array<VDS_Short_String, 32> enemy_texture;
+    VDS_Array<int, 32> x_pos;
+    VDS_Array<int, 32> y_pos;
+};
