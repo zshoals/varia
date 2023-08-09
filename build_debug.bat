e9288737d06c2d4ad262e1ec966e390fdb36f0d4
zshoals <zshoals@gmail.com>
2023-08-09 05:16:57 -0500

    Speed up compilation by calling vcvarsall before vscode startup
    By setting the environment variables before code startup, compilation is WAY faster as vcvarsxxxbat takes literally half the build time. So we remove that with this change.

--- build_debug.bat
+++ build_debug.bat
@@ -1,2 +1,2 @@
-@call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+:: @call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 @MSBuild.exe "C:\Users\ZPC\Desktop\Programming_Projects\Varia\build\Varia.vcxproj" /warnaserror:C4090,C4133,C4296 /nologo /m /clp:Summary /p:Configuration=Debug,Platform=x64,OutDir="c:\Users\ZPC\Desktop\Programming_Projects\Varia\Deployment/"
