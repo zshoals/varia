769c7849b854172af31e57b876ef4c3926f72080
zshoals <zshoals@gmail.com>
2023-08-10 19:53:53 -0500

    release builds now always remake the build files for ease of use

--- build_release.bat
+++ build_release.bat
@@ -1,2 +1,3 @@
 :: @call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+@call "C:\HaxeLibraries\Kinc\make.bat" windows
 @MSBuild.exe "C:\Users\ZPC\Desktop\Programming_Projects\Varia\build\Varia.vcxproj" /warnaserror:C4090,C4133 /nologo /m /clp:Summary /p:Configuration=Release,Platform=x64,OutDir="c:\Users\ZPC\Desktop\Programming_Projects\Varia\Deployment/"
