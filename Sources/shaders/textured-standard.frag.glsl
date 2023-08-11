893a8bbf8ed48c9324dd6bd6b069d40b6d013c89
zshoals <zshoals@gmail.com>
2023-08-11 06:43:04 -0500

    more messing around with shaders

--- Sources/shaders/textured-standard.frag.glsl
+++ Sources/shaders/textured-standard.frag.glsl
@@ -1,13 +1,21 @@
 #version 450
 
-uniform sampler2D tex;
-in vec2 texCoord;
-in vec4 color;
+in vec2 f_texCoord;
+in vec4 f_color;
+
 out vec4 FragColor;
 
+uniform sampler2D u_tex;
+uniform float u_time;
 
 void main() {
-	vec4 texcolor = texture(tex, texCoord) * color;
-	texcolor.rgb *= color.a;
+	float psTime = clamp((((sin(u_time) + 1.0) / 2.0) + 0.05), 0.10, 1.0);
+	vec2 texer = f_texCoord;
+	float weirder = clamp(psTime * 4096.0, 128.0, 4096.0);
+	texer *= vec2(weirder);
+	texer = round(texer);
+	texer /= vec2(weirder);
+	vec4 texcolor = texture(u_tex, texer) * vec4(vec2(f_color), sin(u_time), 1.0);
+	texcolor.rgb *= f_color.a;
 	FragColor = texcolor;
 }
--- Sources/shaders/textured-standard.vert.glsl
+++ Sources/shaders/textured-standard.vert.glsl
@@ -1,17 +1,18 @@
 #version 450
 
-in vec3 vertexPosition;
-in vec2 vertexUV;
-in vec4 vertexColor;
-uniform mat4 projectionMatrix;
-uniform float vTime;
-out vec2 texCoord;
-out vec4 color;
+in vec2  v_pos;
+in float v_layer;
+in vec2  v_uv;
+in vec4  v_color;
+
+out vec2 f_texCoord;
+out vec4 f_color;
+
+uniform float u_time;
 
 void main() {
-	//gl_Position = projectionMatrix * vec4(vertexPosition, 1.0);
-	float pstime = (sin(vTime) + 1.0) / 2.0;
-	gl_Position = vec4(vertexPosition.x, -vertexPosition.y + (pstime * 0.5), vertexPosition.z, 1.0);
-	texCoord = vertexUV;
-	color = vertexColor;
+	float pstime = (sin(u_time) + 1.0) / 2.0;
+	gl_Position = vec4(v_pos.x, -v_pos.y + (pstime * 0.5), v_layer, 1.0);
+	f_texCoord = v_uv;
+	f_color = v_color;
 }
